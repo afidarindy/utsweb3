013a87cc58b7ca77dcf6526298b70d7e07e2e00a
Afida Rindy <afidarindy3@gmail.com>
2020-04-15 16:39:10 +0700

    Updates

---
+++ precache-manifest.fa4b45dc76737f06c7d14dc810d4908e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb91e719f0f3dd8fd160d3374daff857",
+    "url": "/utsweb3/index.html"
+  },
+  {
+    "revision": "69a000ea4852a41e2340",
+    "url": "/utsweb3/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "ee315fafe2762d391f93",
+    "url": "/utsweb3/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "69a000ea4852a41e2340",
+    "url": "/utsweb3/static/js/2.1d5c0998.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/utsweb3/static/js/2.1d5c0998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee315fafe2762d391f93",
+    "url": "/utsweb3/static/js/main.d596b3a8.chunk.js"
+  },
+  {
+    "revision": "73825e60968c0bb77292",
+    "url": "/utsweb3/static/js/runtime-main.8b356d58.js"
+  }
+]);
